41ff4a8530fe8f32e90d08e7662273982b14f87f
Nupur304 <nupurbhavsar222@gmail.com>
2022-05-26 13:45:42 -0400

    Added a message to the main class.

--- src/studentrepo/StudentRepo.java
+++ src/studentrepo/StudentRepo.java
@@ -11,7 +11,7 @@ public class StudentRepo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("GitHub interaction");
     }
     
 }
